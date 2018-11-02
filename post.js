30a04dc8bbed73f2edf3e77cfbd8400a34feecc7
Elena <litishia@ya.ru>
2018-11-02 12:55:27 -0400

    polished working code, adding abstraction and OOP

--- post.js
+++ post.js
@@ -1,6 +1,50 @@
 window.addEventListener( "DOMContentLoaded", function() {
   let form = document.querySelector("form");
 
+  function TalkingForms() {
+    let arrayOfInputs = [];
+    let err = document.querySelector('.err'),
+        success = document.querySelector('.success');
+
+
+     this.getTextInputs = function(){
+       let inputs = document.getElementsByTagName('input');
+
+       inputs = Array.from(inputs); //convert HTML Collection to array
+       arrayOfInputs = inputs.filter(function(i){
+         return i.type != "checkbox"
+       });
+     }
+
+     this.showSuccessMessage = function(output){
+       let goodMessage = arrayOfInputs.map(function(i) {
+         i.classList.remove('wrong');
+         err.innerHTML = "";
+         success.innerHTML = output;
+       })
+     }
+
+     this.showErrorMessage = function(){
+       let badMessage = arrayOfInputs.map(function(i) {
+         success.innerHTML = "";
+         i.classList.add('wrong');
+         err.innerHTML = "Wrong username or password!";
+       })
+     }
+
+     this.blockForm = function(){
+       let fieldset = document.querySelector('fieldset');
+        fieldset.disabled = true;
+     }
+
+     this.unblockForm = function(){
+       let fieldset = document.querySelector('fieldset');
+        fieldset.disabled = false;
+     }
+  }
+
+let tForms = new TalkingForms();
+
   form.addEventListener("submit", function (e){
     e.preventDefault();
     let xhr = new XMLHttpRequest();
@@ -9,22 +53,15 @@ window.addEventListener( "DOMContentLoaded", function() {
     xhr.open("POST", "action_page.php"); //or e.target.action
     xhr.onload = function (response) {
       let output = response.target.responseText;
-      let uname = document.getElementById("uname"),
-          psw = document.getElementById("psw"),
-          err = document.querySelector('.err'),
-          success = document.querySelector('.success');
 
-      if (output != "Error"){
-        uname.classList.remove('wrong');
-        psw.classList.remove('wrong');
-        err.innerHTML = "";
-        success.innerHTML = output;
-      } else {
-        success.innerHTML = "";
-        uname.classList.add('wrong');
-        psw.classList.add('wrong');
-        err.innerHTML = "Wrong username or password!";
-      }
+      tForms.getTextInputs();
+      tForms.blockForm();
+        if (output != "Error"){
+          tForms.showSuccessMessage(output);
+        } else {
+          tForms.showErrorMessage();
+        }
+      tForms.unblockForm();
     }
     xhr.send(formData); // sending POST request with user input data
   })
